75e95ae019f39fd19f2524f2505edfec9a88a623
zayabatkhuyag <zayab2112@gmail.com>
2025-04-22 10:50:07 +0100

    Added Simple Print to Test the Process

--- src/githubexample/GitHubExample.java
+++ src/githubexample/GitHubExample.java
@@ -14,7 +14,8 @@ public class GitHubExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println ("Some Test Code");
     }
     
 }
