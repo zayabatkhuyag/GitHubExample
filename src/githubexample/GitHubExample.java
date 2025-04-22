68f18e7bbf97392961483e2d084a030d25f56251
zayabatkhuyag <zayab2112@gmail.com>
2025-04-22 10:34:36 +0100

    First Commit - Empty Project

---
+++ src/githubexample/GitHubExample.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubexample;
+
+/**
+ *
+ * @author zayab
+ */
+public class GitHubExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
